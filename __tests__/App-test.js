cd075c00224f00bd59e301989d62962657f4eef5
devohno <devohno.ko@gmail.com>
2019-10-12 18:20:16 +0900

    Make first test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -6,22 +6,14 @@ import 'react-native';
 import React from 'react';
 import App from '../src/App';
 
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import {Text} from 'react-native';
 
-// Check Jest
-describe('Jest', () => {
-  it('is it working?', () => {
-    const a = 1;
-    expect(a + 1).toBe(2);
-  });
-});
-
-// Check Enzyme
-describe('Enzyme', () => {
-  it('Is it working?', () => {
-    const text = 'some text';
-    const wrapper = shallow(<Text>{text}</Text>);
-    expect(wrapper.text()).toBe(text);
+// test App
+describe('App', () => {
+  // Header Text
+  const wrapper = shallow(<App />);
+  it('is Text visable? ', () => {
+    expect(wrapper.find('Text').contains('ToDo TDD')).toBe(true);
   });
 });
